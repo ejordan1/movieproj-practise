0e000b72c6e883570c624eef407eb7be2b7de20c
Emerson Jordan <emerz210@gmail.com>
2024-01-10 17:41:53 -0800

    begin login functionality

--- backend/index.js
+++ backend/index.js
@@ -27,18 +27,28 @@ app.get("/books", (req,res)=>{
 })
 
 app.post("/books", (req,res)=>{
-    const q = "INSERT INTO books (`title`, `description`, `price`, `cover`) VALUES (?)";
-    const values = [
-        req.body.title,
-        req.body.description,
-        req.body.price,
-        req.body.cover,
-    ];
+    // const q = "INSERT INTO books (`title`, `description`, `price`, `cover`) VALUES (?)";
+    // const values = [
+    //     req.body.title,
+    //     req.body.description,
+    //     "aaa",
+    //     req.body.cover,
+    // ];
 
-    db.query(q, [values], (err, data) => {
-        if(err) return res.json(err)
-        return res.json("book has been created")
-    })
+    // db.query(q, [values], (err, data) => {
+    //     if(err) return res.json(err)
+    //     return res.json("book has been created")
+    // })
+
+    const q = "INSERT INTO users(`username`,`password`) VALUES (?)"
+        const values = [
+            "testemail",
+            "testpass",
+        ]
+        db.query(q,[values], (err,data)=>{
+            if (err) return res.json(err);
+            return res.status(200).json("user has been created.");
+        });
 })
 
 app.put("/books/:id", (req, res)=>{
