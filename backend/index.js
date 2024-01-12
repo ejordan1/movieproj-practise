ada746bb18be0692f8c215dae1eae7da80ba558e
Emerson Jordan <emerz210@gmail.com>
2024-01-11 18:17:17 -0800

    add prelim login functionality

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,19 @@ app.get("/", (req,res)=>{
     res.json("hello this is the backend")
 })
 
+app.post("/login", (req,res)=>{
+        const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)"
+        const values = [
+            "testuser",
+            "testemail",
+            "testpass",
+        ]
+        db.query(q,[values], (err,data)=>{
+            if (err) return res.json(err);
+            return res.status(200).json("user has been created.");
+        });
+})
+
 app.get("/books", (req,res)=>{
     const q = "SELECT * FROM books"
     db.query(q,(err,data)=>{
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,37 +3,36 @@ import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 
 const Login = () => {
-    const [book, setBook] = useState({
-        title:"",
-        description:"",
-        price: null,
-        cover:"",
+    const [userInfo, setUserInfo] = useState({
+        username:"",
+        email:"",
+        password: "",
     });
 
     const navigate = useNavigate()
 
     const handleChange = (e) => {
-        setBook((prev) => ({ ...prev, [e.target.name]: e.target.value}));
+        setUserInfo((prev) => ({ ...prev, [e.target.name]: e.target.value}));
     };
 
     const handleClick = async e => {
         e.preventDefault()
         try{
-            //await axios.post("http://localhost:8800/books", book);
+            const res = await axios.post("http://localhost:8800/login", userInfo);
+            console.log(res);
             navigate("/"); 
         }catch(err){
             console.log(err)
         }
     }
 
-    console.log(book);
+    console.log(userInfo);
 
     return (<div className='form'>
         <h1>Login </h1>
-        <input type="text" placeholder='title' onChange={handleChange} name="title" />
-        <input type="text" placeholder='desc' onChange={handleChange} name="description"/>
-        <input type="number " placeholder='price' onChange={handleChange} name="price"/>
-        <input type="text" placeholder='cover' onChange={handleChange} name="cover"/>
+        <input type="text" placeholder='username' onChange={handleChange} name="username" />
+        <input type="text" placeholder='email' onChange={handleChange} name="email"/>
+        <input type="number " placeholder='password' onChange={handleChange} name="password"/>
 
         <button onClick={handleClick}>Login</button>
     </div>  
